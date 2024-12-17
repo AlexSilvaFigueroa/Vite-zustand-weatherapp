978ca56d17b02e539a44b7d13446f56c32698353
Alex  Silva Figueroa <alexsilvafigueroa@MacBook-Air-de-Alex.local>
2024-12-17 18:58:12 +0000

    feature: create request route and show data on component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
 import { useState, useEffect } from "react";
-import "./App.css";
-import { Button, Container, Text, Flex } from "@chakra-ui/react";
-// import { weatherResponse } from "./mocks/weather-api";
-// import { getLocation } from "./utils";
-// import { useEffect } from "react";
+import { Button, Container, Text, Flex, Input, Heading } from "@chakra-ui/react";
+import { Field } from "./components/ui/field";
 import { useLocation } from "./hooks";
 import { useWeatherStore } from "./store/weather";
+import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
   const { coords, error } = useLocation();
   const fetchWeather = useWeatherStore((state) => state.fetchWeather);
-  const { weatherState } = useWeatherStore((state) => state.weatherState);
+  const { location, current } = useWeatherStore((state) => state.weatherResponse);
 
   const handleButton = () => {
     setCount(count + 1);
   };
 
   useEffect(() => {
-    if (coords) {
-      fetchWeather(coords);
-    } else {
-      console.error(error, "We can't get your location");
-    }
-  }, [coords, error, fetchWeather]);
-
-  console.log(weatherState, "wetaher component state");
+    if (coords) fetchWeather(coords);
+  }, [coords, error]);
 
   return (
     <>
+      <Container h={100} centerContent marginBottom={5} p={2}>
+        <Heading>Search your city</Heading>
+        <Field label="City" required>
+          <Input placeholder="New York" variant="subtle" bg="gray" />
+        </Field>
+      </Container>
       {/* Currently weather container section */}
-      <Container h={200} bg="red" centerContent marginBottom={5}>
+      <Container h={300} bg="red" centerContent marginBottom={5} p={5}>
         <Flex minH="100%" direction="column" justify="center">
-          <Text fontSize="2xl">Beirut</Text>
-          <Text fontSize="6xl">15</Text>
-          <Text fontSize="xl">Clear</Text>
+          <Text fontSize="4xl">{location?.name}</Text>
+          <Text fontSize="2xl">{location?.country}</Text>
+          <Text fontSize="6xl">{Math.round(current?.temp_c)}</Text>
+          <Text fontSize="xl">{current?.condition?.text}</Text>
         </Flex>
       </Container>
       {/* End */}
--- src/store/weather.js
+++ src/store/weather.js
@@ -1,11 +1,20 @@
 import { create } from "zustand";
 import { weatherResponse } from "../mocks/weather-api";
 
-export const useWeatherStore = create((set) => ({
-  weatherState: {},
-  fetchWeather: async (coors) => {
-    console.log(coors, "fetchweather coors");
-    // const weather = { weatherResponse };
-    set({ weatherState: weatherResponse });
-  },
-}));
+export const useWeatherStore = create((set) => {
+  return {
+    initialState: {},
+    weatherResponse,
+
+    fetchWeather: async (coords) => {
+      const res = await fetch(
+        `https://api.weatherapi.com/v1/current.json?q=${coords.latitude}%20${coords.longitude}&key=${
+          import.meta.env.VITE_WEATHER_API_KEY
+        }`
+      );
+      const weatherResponse = await res.json();
+
+      set({ weatherResponse });
+    },
+  };
+});
